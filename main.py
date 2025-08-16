7b50c968c552ede2550c64007efa2750d8c67c83
GTDae <gerhard.thirion@gmail.com>
2025-08-16 16:48:25 +0200

    J: Performed a code audit, and applied bug fixes to main.py and asteroidfield.py.

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable,)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroid_field = AsteroidField()
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -33,11 +33,12 @@ def main():
 
         screen.fill("black")
 
-        for thing in drawable:
-            thing.draw(screen)
-        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
