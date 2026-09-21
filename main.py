efddbc8b1b8fff78594e068e3d63aea305a82b6d
Gerhard Thirion <gerhard.thirion@gmail.com>
2026-09-21 14:07:25 +0200

    V: Restore missing AsteroidField instantiation so asteroids spawn again

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     Player.containers = (updatable, drawable)
     Explosion.containers = (explosions, updatable, drawable)
 
